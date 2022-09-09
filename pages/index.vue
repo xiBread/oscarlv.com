d762acbc3b95d10451ffb63b8eaa85fa862afeb3
Oscar Lee-Vermeren <oscar.leevermeren@gmail.com>
2022-09-09 16:39:15 -0400

    steve

--- pages/index.vue
+++ pages/index.vue
@@ -1,5 +1,31 @@
+<!-- eslint-disable no-mixed-spaces-and-tabs -->
 <template>
-	<div class="h-screen"></div>
+	<div class="h-screen">
+		<div class="flex h-full items-center justify-center">
+			<pre>
+ _________________________________________
+/ Hi, I'm Steve the Stegosaurus!          \
+|                                         |
+| I'm taking place of the home page until |
+| my creator decides what he wants to put |
+\ here.                                   /
+ -----------------------------------------
+\                             .       .
+ \                           / `.   .' "
+  \                  .---.  &lt;    > &lt;    >  .---.
+   \                 |    \  \ - ~ ~ - /  /    |
+         _____          ..-~             ~-..-~
+        |     |   \~~~\.'                    `./~~~/
+       ---------   \__/                        \__/
+      .'  O    \     /               /       \  "
+     (_____,    `._.'               |         }  \/~~~/
+      `----.          /       }     |        /    \__/
+            `-.      |       /      |       /      `. ,~~|
+                ~-.__|      /_ - ~ ^|      /- _      `..-'
+                     |     /        |     /     ~-.     `-. _  _  _
+                     |_____|        |_____|         ~ - . _ _ _ _ _>
+			<!-- prettier-ignore -->
+			</pre>
+		</div>
+	</div>
 </template>
-
-<script setup lang="ts"></script>
