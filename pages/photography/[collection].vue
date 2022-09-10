401c0d9102ad00946d2b1fdee30b88966d9ce049
Oscar Lee-Vermeren <oscar.leevermeren@gmail.com>
2022-09-10 00:56:37 -0400

    setup collection page

--- pages/photography/[collection].vue
+++ pages/photography/[collection].vue
@@ -1,13 +1,16 @@
 <template>
-	<div></div>
+	<div class="prose dark:prose-invert">
+		<ContentDoc tag="article" :path="`/photography/${collection}`" />
+	</div>
 </template>
 
 <script setup lang="ts">
 const route = useRoute();
+const collection = route.params.collection;
 
 const photos = await useFetch("/api/assets", {
 	params: {
-		collection: route.params.collection,
+		collection,
 	},
 });
 </script>
