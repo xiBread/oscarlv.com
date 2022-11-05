7b5f10b05fca10eac06d0e0556367f6d8665a1a2
Oscar Lee-Vermeren <oscar.leevermeren@gmail.com>
2022-11-04 22:54:45 -0400

    ap

---
+++ pages/art/ap.vue
@@ -0,0 +1,7 @@
+<template>
+	<div class="mx-auto mt-44 flex h-full max-w-4xl justify-center">
+		<ContentDoc tag="article" class="prose text-justify dark:prose-invert" />
+	</div>
+</template>
+
+<script setup lang="ts"></script>
---
+++ pages/art/index.vue
@@ -0,0 +1,16 @@
+<template>
+	<div
+		class="prose flex h-full max-w-none items-center justify-center font-mono prose-a:font-normal prose-a:no-underline dark:prose-invert"
+	>
+		<ol class="[&>li]:marker:text-neutral-500">
+			<li>
+				<NuxtLink to="/art/ap">AP Art</NuxtLink>
+			</li>
+			<li>
+				<NuxtLink to="/art/design">Graphic/Digital Design 3</NuxtLink>
+			</li>
+		</ol>
+	</div>
+</template>
+
+<script setup lang="ts"></script>
