1ac12d03b33581d9afd1ffaab867a2f64fbda174
Oscar Lee-Vermeren <oscar.leevermeren@gmail.com>
2022-11-19 10:45:10 -0500

    refactor: move app config into nuxt config

--- app.config.ts
+++
@@ -1,7 +0,0 @@
-export default defineAppConfig({
-	socials: {
-		Twitter: "https://www.twitter.com/oleevermeren",
-		Instagram: "https://www.instagram.com/isocausality",
-		GitHub: "https://www.github.com/xiBread",
-	},
-});
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -66,6 +66,13 @@ export default defineNuxtConfig({
 			],
 		},
 	},
+	appConfig: {
+		socials: {
+			Twitter: "https://www.twitter.com/oleevermeren",
+			Instagram: "https://www.instagram.com/isocausality",
+			GitHub: "https://www.github.com/xiBread",
+		},
+	},
 	typescript: {
 		shim: false,
 	},
