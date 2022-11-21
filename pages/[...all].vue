3d41f863d99447c0384d26c3d51abe40b4515554
Oscar Lee-Vermeren <oscar.leevermeren@gmail.com>
2022-11-21 12:37:02 -0500

    feat: add 404 page

---
+++ pages/[...all].vue
@@ -0,0 +1,14 @@
+<template>
+	<div class="flex h-screen items-center justify-center">
+		<h1 class="mr-5 border-r border-neutral-600 pr-5 text-2xl font-semibold leading-relaxed">
+			404
+		</h1>
+
+		<h2 class="text-lg">Page not found</h2>
+	</div>
+</template>
+
+<script setup lang="ts">
+useHead({ title: "404" });
+setResponseStatus(404);
+</script>
