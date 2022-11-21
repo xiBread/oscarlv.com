b4b7b65b44a53942ea10b205d1b848a93033a1e2
Oscar Lee-Vermeren <oscar.leevermeren@gmail.com>
2022-11-20 21:39:50 -0500

    feat: style writing slug page

--- pages/writing/[slug].vue
+++ pages/writing/[slug].vue
@@ -1,3 +1,9 @@
 <template>
-	<div></div>
+	<div class="mt-16 sm:px-8 lg:mt-28">
+		<Container>
+			<div class="mx-auto max-w-2xl">
+				<ContentDoc tag="article" class="use-prose" />
+			</div>
+		</Container>
+	</div>
 </template>
