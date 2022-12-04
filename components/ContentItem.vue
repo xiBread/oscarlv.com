83ee8b4978438f24aa0736fe869cbe4e2ec398e1
Oscar Lee-Vermeren <oscar.leevermeren@gmail.com>
2022-12-03 20:04:02 -0500

    refactor(ContentItem): remove redundant `$props`

--- components/ContentItem.vue
+++ components/ContentItem.vue
@@ -4,7 +4,7 @@
 			class="absolute -inset-y-6 -inset-x-4 z-0 bg-neutral-200 opacity-0 transition-opacity group-hover:opacity-100 dark:bg-neutral-800 sm:-inset-x-6 sm:rounded-2xl"
 		></div>
 
-		<NuxtLink :to="`${$route.path}/${$props.item.slug}`">
+		<NuxtLink :to="`${$route.path}/${item.slug}`">
 			<span class="absolute -inset-y-6 -inset-x-4 z-20 sm:-inset-x-6 sm:rounded-2xl"></span>
 			<span class="relative z-10">{{ item.title }}</span>
 		</NuxtLink>
