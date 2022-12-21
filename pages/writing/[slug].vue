4560dc557727c14a459a0ac7899b8d6309e782ec
Oscar Lee-Vermeren <oscar.leevermeren@gmail.com>
2022-12-21 13:02:56 -0500

    fix: preserve whitespace

--- pages/writing/[slug].vue
+++ pages/writing/[slug].vue
@@ -2,7 +2,10 @@
 	<div class="mt-16 sm:px-8 lg:mt-28">
 		<Container>
 			<div class="mx-auto max-w-3xl">
-				<article class="use-prose !max-w-none" v-html="body"></article>
+				<article
+					class="use-prose !max-w-none [&_p]:whitespace-pre-wrap"
+					v-html="body"
+				></article>
 			</div>
 		</Container>
 	</div>
