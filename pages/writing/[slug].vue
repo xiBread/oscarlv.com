beff75c100b911ff5d61e63c6bc96de147a70524
Oscar Lee-Vermeren <oscar.leevermeren@gmail.com>
2022-12-21 02:51:50 -0500

    fix: render rich text assets

--- pages/writing/[slug].vue
+++ pages/writing/[slug].vue
@@ -2,13 +2,14 @@
 	<div class="mt-16 sm:px-8 lg:mt-28">
 		<Container>
 			<div class="mx-auto max-w-3xl">
-				<article class="use-prose !max-w-none" v-html="render(entry.body)"></article>
+				<article class="use-prose !max-w-none" v-html="body"></article>
 			</div>
 		</Container>
 	</div>
 </template>
 
 <script setup lang="ts">
+import { BLOCKS } from "@contentful/rich-text-types";
 import type { WritingEntry } from "~/util/types";
 
 const route = useRoute();
@@ -16,6 +17,14 @@ const { getSingleEntry, render } = useContentful();
 
 const entry = await getSingleEntry<WritingEntry>("writing", {
 	"fields.slug": route.params.slug,
+});
+
+const body = render(entry.body, {
+	renderNode: {
+		[BLOCKS.EMBEDDED_ASSET]: (node) => {
+			return `<img src="${node.data.target.fields.file.url}" />`;
+		},
+	},
 });
 
 useEntryHead(entry);
