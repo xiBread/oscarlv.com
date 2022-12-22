5a0b8a2f1f0a5da5774025695c0d9c19de86897e
Oscar Lee-Vermeren <oscar.leevermeren@gmail.com>
2022-12-21 20:44:51 -0500

    refactor(writing): update layout

--- pages/writing/index.vue
+++ pages/writing/index.vue
@@ -3,29 +3,40 @@
 		<Container>
 			<header class="use-prose max-w-2xl" v-html="render(page.body)"></header>
 
-			<div class="mt-16 sm:mt-20">
-				<ul class="grid grid-cols-2 gap-x-12 gap-y-16 sm:grid-cols-3 lg:grid-cols-4">
-					<li
-						v-for="entry in entries"
-						:key="entry.slug"
-						class="group relative flex flex-col items-start"
+			<div class="mt-20 space-y-28 sm:mt-28">
+				<div v-for="(entries, category) in groups" :key="category">
+					<ul
+						class="relative grid grid-cols-2 gap-x-6 gap-y-10 sm:grid-cols-3 lg:grid-cols-5"
 					>
-						<ContentItem :entry="entry">
-							<dl>
-								<dt class="sr-only">Category</dt>
-								<dd class="description flex items-center">
-									{{ entry.category }}
+						<span
+							class="absolute -top-14 -left-px select-none text-4xl font-bold text-neutral-500/30"
+						>
+							{{ category }}
+						</span>
+
+						<li
+							v-for="entry in entries"
+							:key="entry.slug"
+							class="group relative flex flex-col items-start"
+						>
+							<NuxtLink
+								:to="`${$route.path}/${entry.slug}`"
+								:title="entry.title + (entry.explicit ? ' (Explicit)' : '')"
+								class="text-base font-semibold text-black dark:text-white"
+							>
+								<div class="relative z-10 flex items-center">
+									{{ entry.title }}
 
 									<Icon
 										v-if="entry.explicit"
 										icon="material-symbols:explicit"
-										class="ml-2 h-4 w-4"
+										class="ml-2 h-4 w-4 text-neutral-600 dark:text-neutral-400"
 									/>
-								</dd>
-							</dl>
-						</ContentItem>
-					</li>
-				</ul>
+								</div>
+							</NuxtLink>
+						</li>
+					</ul>
+				</div>
 			</div>
 		</Container>
 	</div>
@@ -45,5 +56,11 @@ const { items } = await getEntries<WritingEntry>({
 	order: "fields.category,fields.title",
 });
 
-const entries = items.map((entry) => entry.fields);
+const groups = items
+	.map((entry) => entry.fields)
+	.reduce<Record<string, WritingEntry[]>>((group, entry) => {
+		(group[entry.category] ??= []).push(entry);
+
+		return group;
+	}, {});
 </script>
