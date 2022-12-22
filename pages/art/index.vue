3af6eb24865fa227e53fd6ee2dc68e77106b7f1f
Oscar Lee-Vermeren <oscar.leevermeren@gmail.com>
2022-12-21 20:47:56 -0500

    refactor(art): consolidate template

--- pages/art/index.vue
+++ pages/art/index.vue
@@ -19,9 +19,25 @@
 							loading="lazy"
 						/>
 
-						<ContentItem :entry="entry">
-							<p class="description">{{ entry.description }}</p>
-						</ContentItem>
+						<div class="text-base font-semibold text-black dark:text-white">
+							<div
+								class="absolute -inset-y-6 -inset-x-4 z-0 bg-neutral-200 opacity-0 transition-opacity group-hover:opacity-100 dark:bg-neutral-800 sm:-inset-x-6 sm:rounded-2xl"
+							></div>
+
+							<NuxtLink :to="`${$route.path}/${entry.slug}`" :title="entry.title">
+								<span
+									class="absolute -inset-y-6 -inset-x-4 z-20 sm:-inset-x-6 sm:rounded-2xl"
+								></span>
+
+								<span class="relative z-10">{{ entry.title }}</span>
+							</NuxtLink>
+						</div>
+
+						<p
+							class="relative z-10 mt-2 text-sm text-neutral-600 dark:text-neutral-400"
+						>
+							{{ entry.description }}
+						</p>
 					</li>
 				</ul>
 			</div>
