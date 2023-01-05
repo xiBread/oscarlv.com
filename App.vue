924b4bb0e7cf866d6f5869b89669f5c1f11cc05a
Oscar Lee-Vermeren <oscar.leevermeren@gmail.com>
2023-01-05 01:48:16 -0500

    refactor: move head back into `App.vue`

--- App.vue
+++ App.vue
@@ -1,5 +1,23 @@
 <template>
 	<Html lang="en">
+		<Head>
+			<Meta hid="light" name="theme-color" content="#ffffff" />
+			<Meta
+				hid="dark"
+				name="theme-color"
+				content="#0a0a0a"
+				media="(prefers-color-scheme: dark)"
+			/>
+
+			<Meta property="og:type" content="website" />
+
+			<Link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
+			<Link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
+
+			<Link rel="preconnect" href="https://rsms.me" />
+			<Link rel="stylesheet" href="https://rsms.me/inter/inter.css" />
+		</Head>
+
 		<Body class="flex h-full flex-col dark:bg-[#0a0a0a] dark:text-white">
 			<Lichtenberg />
 
@@ -17,6 +35,8 @@
 </template>
 
 <script setup lang="ts">
+useHead({ script: [{ src: "/theme.js", type: "module" }] });
+
 if (typeof process !== "undefined") {
 	process.browser = true;
 }
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,55 +1,6 @@
 export default defineNuxtConfig({
 	modules: ["@nuxt/image", "@nuxtjs/tailwindcss", "@vueuse/nuxt"],
 	css: ["~/assets/main.css"],
-	app: {
-		head: {
-			meta: [
-				{
-					key: "tc-light",
-					name: "theme-color",
-					content: "#ffffff",
-				},
-				{
-					key: "tc-dark",
-					name: "theme-color",
-					content: "#171717",
-					media: "(prefers-color-scheme: dark)",
-				},
-				{
-					property: "og:type",
-					content: "website",
-				},
-			],
-			link: [
-				{
-					rel: "icon",
-					type: "image/png",
-					sizes: "16x16",
-					href: "/favicon-16x16.png",
-				},
-				{
-					rel: "icon",
-					type: "image/png",
-					sizes: "32x32",
-					href: "/favicon-32x32.png",
-				},
-				{
-					rel: "preconnect",
-					href: "https://rsms.me",
-				},
-				{
-					rel: "stylesheet",
-					href: "https://rsms.me/inter/inter.css",
-				},
-			],
-			script: [
-				{
-					src: "/theme.js",
-					type: "module",
-				},
-			],
-		},
-	},
 	nitro: {
 		prerender: {
 			ignore: ["/art", "/writing"],
