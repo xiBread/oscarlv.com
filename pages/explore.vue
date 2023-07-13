65035e9cd73fe67b500ada09b8553e768f64ecee
Oscar Lee-Vermeren <oscar.leevermeren@gmail.com>
2023-07-13 19:20:50 -0400

    refactor: increase hover opacity

--- pages/explore.vue
+++ pages/explore.vue
@@ -18,7 +18,7 @@
 						>
 							<div class="font-semibold text-zinc-800 dark:text-zinc-100">
 								<div
-									className="absolute -inset-x-4 -inset-y-6 z-0 scale-95 bg-zinc-50 opacity-0 transition group-hover:scale-100 group-hover:opacity-70 dark:bg-zinc-800/50 sm:-inset-x-6 sm:rounded-2xl"
+									className="absolute -inset-x-4 -inset-y-6 z-0 scale-95 bg-zinc-50 opacity-0 transition group-hover:scale-100 group-hover:opacity-100 dark:bg-zinc-800/70 sm:-inset-x-6 sm:rounded-2xl"
 								/>
 
 								<NuxtLink
