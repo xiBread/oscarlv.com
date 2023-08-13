c01f4af8081a09e04e1125494309fe81bbf0be90
Oscar Lee-Vermeren <oscar.leevermeren@gmail.com>
2023-08-13 09:59:46 -0400

    refactor: remove quotes

--- pages/index.vue
+++ pages/index.vue
@@ -7,27 +7,10 @@
 		>
 			<h1>Curiosity through the eyes of expression.</h1>
 
-			<div class="not-prose">
-				<blockquote class="mb-2 whitespace-pre-wrap italic">
-					"{{ quote?.content }}"
-				</blockquote>
-
-				<p class="text-zinc-500 dark:text-zinc-400">
-					&mdash; {{ quote?.quotee + (quote?.source ? ", " : "") }}
-
-					<cite v-if="quote?.source" class="font-medium">
-						<NuxtLink
-							v-if="typeof quote.source === 'object'"
-							:href="quote.source.url"
-							class="underline decoration-zinc-500 underline-offset-2 hover:text-white"
-						>
-							{{ quote.source.text }}
-						</NuxtLink>
-
-						<template v-else>{{ quote.source }}</template>
-					</cite>
-				</p>
-			</div>
+			<p>
+				Lorem ipsum dolor sit amet consectetur adipisicing elit. Quod dignissimos eligendi
+				nihil tenetur cum maxime similique a, dolores nostrum fugit aspernatur voluptatem.
+			</p>
 		</div>
 	</Container>
 
@@ -35,15 +18,5 @@
 </template>
 
 <script setup lang="ts">
-import type json from "../content/quotes.json";
-
-type Quote = (typeof json)[number];
-
-const quote = ref<Quote>();
-
-const { data } = await useAsyncData(() => queryContent("quotes").findOne());
-const quotes = (data.value?.body ?? []) as Quote[];
-
-onMounted(() => (quote.value = quotes[(Math.random() * quotes.length) | 0]));
 onKeyStroke("Enter", () => useRouter().push("/explore"));
 </script>
