d6c3ebbdb4b549df12465d50744602a1aa4e9e04
xiBread <oscar.leevermeren@gmail.com>
2023-08-28 10:34:46 -0400

    refactor: `shared` -> `base`

--- src/content/config.ts
+++ src/content/config.ts
@@ -1,18 +1,18 @@
 import { defineCollection, z } from "astro:content";
 
-const shared = z.object({ title: z.string() });
+const base = z.object({ title: z.string() });
 
 export const collections = {
 	art: defineCollection({
 		type: "content",
-		schema: shared.extend({
+		schema: base.extend({
 			description: z.string(),
 			cover: z.string().startsWith("/"),
 		}),
 	}),
 	writing: defineCollection({
 		type: "content",
-		schema: shared.extend({
+		schema: base.extend({
 			excerpt: z.string().optional(),
 			start: z.enum(["even", "odd"]).optional(),
 			explicit: z.boolean().optional(),
@@ -21,6 +21,6 @@ export const collections = {
 	}),
 	single: defineCollection({
 		type: "content",
-		schema: shared,
+		schema: base,
 	}),
 };
