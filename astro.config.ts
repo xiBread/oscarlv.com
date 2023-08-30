6d2efae7f5a62cb48cd57392ac7ea9ad5cd1f9eb
Oscar Lee-Vermeren <oscar.leevermeren@gmail.com>
2023-08-30 15:12:08 -0400

    fix: explicit image service

--- astro.config.ts
+++ astro.config.ts
@@ -2,15 +2,15 @@ import mdx from "@astrojs/mdx";
 import tailwind from "@astrojs/tailwind";
 import vercel from "@astrojs/vercel/serverless";
 import vue from "@astrojs/vue";
-import { defineConfig } from "astro/config";
+import { defineConfig, sharpImageService } from "astro/config";
 
 // https://astro.build/config
 export default defineConfig({
 	integrations: [tailwind(), vue(), mdx()],
 	output: "hybrid",
 	adapter: vercel(),
-	// @ts-expect-error
 	image: {
+		service: sharpImageService(),
 		domains: ["ik.imagekit.io"],
 	},
 });
