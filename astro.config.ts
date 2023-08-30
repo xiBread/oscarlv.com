3ee96ec911dcbb75f24c44fa7075019490ab1ec5
Oscar Lee-Vermeren <oscar.leevermeren@gmail.com>
2023-08-30 15:46:47 -0400

    feat: astro v3

--- astro.config.ts
+++ astro.config.ts
@@ -2,19 +2,15 @@ import mdx from "@astrojs/mdx";
 import tailwind from "@astrojs/tailwind";
 import vercel from "@astrojs/vercel/serverless";
 import vue from "@astrojs/vue";
-import { defineConfig, sharpImageService } from "astro/config";
+import { defineConfig } from "astro/config";
 
 // https://astro.build/config
 export default defineConfig({
 	integrations: [tailwind(), vue(), mdx()],
 	output: "hybrid",
 	adapter: vercel(),
+	// @ts-expect-error
 	image: {
-		service: sharpImageService(),
 		domains: ["ik.imagekit.io"],
-	},
-	experimental: {
-		assets: true,
-		viewTransitions: true,
 	},
 });
