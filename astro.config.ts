9032b6b9e76a806433de0c3d660b908edca98a18
Oscar Lee-Vermeren <oscar.leevermeren@gmail.com>
2023-09-06 00:52:33 -0400

    fix: add image config

--- astro.config.ts
+++ astro.config.ts
@@ -4,11 +4,19 @@ import vercel from "@astrojs/vercel/serverless";
 import vue from "@astrojs/vue";
 import { defineConfig } from "astro/config";
 
+const domains = ["ik.imagekit.io"];
+
 // https://astro.build/config
 export default defineConfig({
 	integrations: [tailwind(), vue(), mdx()],
 	output: "hybrid",
-	adapter: vercel({ imageService: true }),
+	adapter: vercel({
+		imagesConfig: {
+			domains,
+			sizes: [1080, 2048],
+		},
+		imageService: true,
+	}),
 	// @ts-expect-error
 	image: {
 		domains: ["ik.imagekit.io"],
