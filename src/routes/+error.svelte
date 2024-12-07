011554f25700f17a9dafab69c626f4b08f2752ea
Oscar Lee-Vermeren <oscar.leevermeren@gmail.com>
2024-12-07 18:05:38 -0500

    feat: add basic error page

---
+++ src/routes/+error.svelte
@@ -0,0 +1,20 @@
+<script lang="ts">
+	import { page } from "$app/stores";
+</script>
+
+{#if $page.error}
+	<div class="flex-center flex h-svh flex-col px-4">
+		<h1>
+			<span class="text-vintage-peach font-mono">{$page.status}</span> | {$page.error.message}
+		</h1>
+
+		<button
+			class="group mt-6 hover:cursor-pointer"
+			type="button"
+			onclick={() => history.back()}
+		>
+			<span class="inline-block transition-transform group-hover:-translate-x-1">&larr;</span>
+			Back
+		</button>
+	</div>
+{/if}
